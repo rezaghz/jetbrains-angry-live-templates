05a932deb33dffb4c9e85e4b11011ceb0ea21fa0
Reza Ghasemzadeh <36597017+rezaghz@users.noreply.github.com>
2022-05-20 13:50:42 +0430

    add new context

---
+++ src/main/java/com/rezaghz/liveTemplates/LaravelContext.java
@@ -0,0 +1,20 @@
+// Copyright 2000-2022 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package com.rezaghz.liveTemplates;
+
+import com.intellij.codeInsight.template.TemplateActionContext;
+import com.intellij.codeInsight.template.TemplateContextType;
+import org.jetbrains.annotations.NotNull;
+
+public class LaravelContext extends TemplateContextType {
+
+    protected LaravelContext() {
+        super("LARAVELFILES", "AngryLiveTemplates");
+    }
+
+    @Override
+    public boolean isInContext(@NotNull TemplateActionContext templateActionContext) {
+        return templateActionContext.getFile().getName().endsWith(".blade.php");
+    }
+
+}
